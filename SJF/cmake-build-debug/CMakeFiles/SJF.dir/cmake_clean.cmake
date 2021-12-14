b1e4d5a7224a7de0c4a677c1b15a4aba08216923
mina-beiki <mina_beiki@yahoo.com>
2021-12-14 21:02:58 +0330

    added SJF

---
+++ SJF/cmake-build-debug/CMakeFiles/SJF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SJF.dir/main.c.o"
+  "SJF"
+  "SJF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SJF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
